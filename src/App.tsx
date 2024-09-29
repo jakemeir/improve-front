bc629c825557fa17e058d6de8c0b9a7bb54338ed
azriel <azrielsch61@gmail.com>
2024-09-29 12:43:49 +0300

    react

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CounterTest from './components/CounterTest';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <CounterTest/>
+    </div>
+  );
+};
+
+export default App;
