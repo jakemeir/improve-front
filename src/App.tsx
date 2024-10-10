dcc0468d0de87e439bc1e8016cd703942eb1d93a
isaacY0573 <isaacjosef53@gmail.com>
2024-10-10 13:46:25 +0300

    marging layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
 import CreateUser from './components/CreateUser';
 import UpdateUser from './components/UpdateUser';
-import './index.css'
-
+import './index.css';
+// import { Layout } from 'lucide-react';
+import Layout from './components/Layout';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Updated import
+import SearchBox from './components/SearchBox';
+import UsersPage from './components/UsersPage';
 
 const App: React.FC = () => {
-
-//props for update user
-const [isModelOpen, setModelOpen] = useState(false);
+  // Props for update user
+  const [isModelOpen, setModelOpen] = useState(false);
 
   // An example of a user in the list
   const someUserData = {
@@ -17,22 +20,20 @@ const [isModelOpen, setModelOpen] = useState(false);
     phone: "0541234567",
     password: "password123",
     role: "user"
-};
-
+  };
 
   return (
     <div>
-        <Router>
-    <Layout>
-      <Routes>
-        <Route path="/users" element={<UsersPage />} />
-        {/* Add other routes as needed */}
-        <Route path="/" element={<div>IMPROVE PROJECT 2024</div>} />
-      </Routes>
-    </Layout>
-  </Router>
+      <Router> {/* Wrap everything inside BrowserRouter */}
+        <Layout>
+          <Routes>
+            <Route path="/users" element={<UsersPage />} />
+            <Route path="/" element={<div>IMPROVE PROJECT 2024</div>} />
+          </Routes>
+        </Layout>
+      </Router>
       <SearchBox />
-      <CreateUser/>
+      {/* <CreateUser /> */}
     </div>
   );
 };
