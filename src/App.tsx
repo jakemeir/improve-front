e63eb339ba026b05a8d359e9b2e46303cd0be277
isaacY0573 <isaacjosef53@gmail.com>
2024-10-10 10:23:46 +0300

    added export to exel

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import CreateUser from './components/CreateUser';
 import './index.css'
+import ExportUserButton from './components/ExportUserButton';
 
 
 const App: React.FC = () => {
   return (
     <div>
       <CreateUser/>
+      {/* <ExportUserButton/> */}
+      <a href='http://localhost:8080/users/export'>export</a>
     </div>
   );
 };
