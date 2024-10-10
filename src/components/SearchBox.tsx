c4ae6999cc6317d2c80076837e8914d0dc91e8b9
isaacY0573 <isaacjosef53@gmail.com>
2024-10-10 15:01:37 +0300

    update 0.02 layout

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -34,7 +34,7 @@ import '../style/SearchBox.css'
     };
   return (
     <div className='search-modal'>
-      <label htmlFor='search-input'>Search</label>
+      <label htmlFor='search-input'></label>
       <div className="input-container">
         <input type="text" placeholder="Search..." value={searchTerm} onChange={handleInputChange} className="search-input"/>
         <button onClick={handleSearch} className="search-button"> search </button>
