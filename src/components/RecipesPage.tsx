8dedb66fb54e3fa3619b3fc004989b650f02b496
jakemeir <jakemeirjacobs@gmail.com>
2024-10-28 11:57:31 +0200

    Delete recipe

--- src/components/RecipesPage.tsx
+++ src/components/RecipesPage.tsx
@@ -5,7 +5,7 @@ import { Recipe } from '../types/types';
 import axios from 'axios';
 import Cookies from 'js-cookie';
 // import UpdateRecipe from './UpdateRecipe';
-// import DeleteRecipe from './DeleteRecipe';
+import DeleteRecipe from './DeleteRecipe'
 import ExportRecipe from './ExportRecipe';
 
 const RecipePage = () => {
@@ -106,8 +106,8 @@ const RecipePage = () => {
           ))}
         </tbody>
       </table>
-      {/* {isEditOpen && <UpdateRecipe onSuccess={fetchRecipeData} recipeId={recipeId} onClose={() => handleEditClick(recipeId)} isOpen={isEditOpen} />}
-      {isDeleteOpen && <DeleteRecipe onSuccess={fetchRecipeData} onClose={() => handleDeleteClick(recipeId)} recipeId={recipeId} />} */}
+      {/* {isEditOpen && <UpdateRecipe onSuccess={fetchRecipeData} recipeId={recipeId} onClose={() => handleEditClick(recipeId)} isOpen={isEditOpen} />} */}
+      {isDeleteOpen && <DeleteRecipe onSuccess={fetchRecipeData} onClose={() => handleDeleteClick(recipeId)} recipeId={recipeId} />}
     </div>
   )
 }
